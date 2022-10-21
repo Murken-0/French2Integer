a3a48e60a00a66a561f2b5afb8e9a0526e17f5bf
Vlad <muravievvlad.2002@gmail.com>
2022-10-21 23:35:23 +0300

    change label position

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@
 			this.inputBox.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.inputBox.Location = new System.Drawing.Point(30, 64);
 			this.inputBox.Name = "inputBox";
-			this.inputBox.Size = new System.Drawing.Size(577, 32);
+			this.inputBox.Size = new System.Drawing.Size(577, 27);
 			this.inputBox.TabIndex = 0;
 			this.inputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
 			// 
@@ -47,9 +47,9 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(29, 31);
+			this.label1.Location = new System.Drawing.Point(96, 31);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(578, 33);
+			this.label1.Size = new System.Drawing.Size(441, 25);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Введите словами число на французском для перевода в число";
 			// 
